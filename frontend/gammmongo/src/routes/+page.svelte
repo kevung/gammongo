8ad39233ef1b0621bf458f432cdc3a00bfca3e8b
Kévin Unger <kevin.unger@proton.me>
2025-04-29 01:07:52 +0200

    install accordion component

--- frontend/gammmongo/src/routes/+page.svelte
+++ frontend/gammmongo/src/routes/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
-  import { Button } from "$lib/components/ui/button/index.js";
+import { Button } from "$lib/components/ui/button/index.js";
+ import * as Accordion from "$lib/components/ui/accordion/index.js";
 </script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
 <Button> Click me</Button>
+
+<Accordion.Root type="single">
+    <Accordion.Item value="item-1">
+        <Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+        <Accordion.Content>
+            Yes. It adheres to the WAI-ARIA design pattern.
+        </Accordion.Content>
+    </Accordion.Item>
+</Accordion.Root>

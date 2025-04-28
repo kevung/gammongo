15b85f94e03277462c1f0125c2b5a13992526ecb
Kévin Unger <kevin.unger@proton.me>
2025-04-29 01:19:30 +0200

    install component card and carousel

--- frontend/gammmongo/src/routes/+page.svelte
+++ frontend/gammmongo/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
-import { Button } from "$lib/components/ui/button/index.js";
- import * as Accordion from "$lib/components/ui/accordion/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Accordion from "$lib/components/ui/accordion/index.js";
+    import * as Card from "$lib/components/ui/card/index.js";
+    import * as Carousel from "$lib/components/ui/carousel/index.js";
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -16,3 +18,23 @@ import { Button } from "$lib/components/ui/button/index.js";
         </Accordion.Content>
     </Accordion.Item>
 </Accordion.Root>
+
+<Carousel.Root class="w-full max-w-xs">
+    <Carousel.Content>
+        {#each Array(5) as _, i (i)}
+            <Carousel.Item>
+                <div class="p-1">
+                    <Card.Root>
+                        <Card.Content
+                            class="flex aspect-square items-center justify-center p-6"
+                        >
+                            <span class="text-4xl font-semibold">{i + 1}</span>
+                        </Card.Content>
+                    </Card.Root>
+                </div>
+            </Carousel.Item>
+        {/each}
+    </Carousel.Content>
+    <Carousel.Previous />
+    <Carousel.Next />
+</Carousel.Root>
